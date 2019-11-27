a6c9c81f15a3c568629336efd913ebd94f40694c
Tantacula <bonesbrain@gmail.com>
2019-11-27 06:22:47 +0300

    Добавлены модель Place и миграция

---
+++ database/migrations/2019_11_27_025634_create_places_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlacesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('places', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->point('point');
+            $table->unsignedBigInteger('user_id');
+            $table->string('comment');
+
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                  ->references('id')
+                  ->on('users')
+                  ->onDelete('cascade');
+            $table->spatialIndex('point');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('places');
+    }
+}
