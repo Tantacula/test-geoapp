aa7fddc8b06771e11719a57938b660592208eee9
Tantacula <bonesbrain@gmail.com>
2019-11-28 05:50:40 +0300

    Фикс иконок маркеров

--- resources/js/app.js
+++ resources/js/app.js
@@ -14,9 +14,9 @@ import 'leaflet/dist/leaflet.css'
 delete Icon.Default.prototype._getIconUrl;
 
 Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+  iconRetinaUrl: 'images/leaflet/marker-icon-2x.png',
+  iconUrl: 'images/leaflet/marker-icon.png',
+  shadowUrl: 'images/leaflet/marker-shadow.png'
 });
 
 Vue.config.productionTip = false
